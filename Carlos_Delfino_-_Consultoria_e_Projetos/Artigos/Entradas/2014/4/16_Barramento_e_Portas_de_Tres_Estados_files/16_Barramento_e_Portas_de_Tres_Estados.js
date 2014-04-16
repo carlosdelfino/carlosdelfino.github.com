de5aaa17047b8b894fcc0a9c6d73ebcf68121851
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2014-04-16 18:00:45 -0300

    Mais artigos,
    Todos são espelho do que está no Facebook no Curso Arduino Minas.
    Em Breve mais informações sobre links.

---
+++ Carlos_Delfino_-_Consultoria_e_Projetos/Artigos/Entradas/2014/4/16_Barramento_e_Portas_de_Tres_Estados_files/16_Barramento_e_Portas_de_Tres_Estados.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20140416
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,264),url:'16_Barramento_e_Portas_de_Tres_Estados_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'16_Barramento_e_Portas_de_Tres_Estados_files/stroke_1.png'},{rect:new IWRect(2,-2,417,4),url:'16_Barramento_e_Portas_de_Tres_Estados_files/stroke_2.png'},{rect:new IWRect(419,-2,5,4),url:'16_Barramento_e_Portas_de_Tres_Estados_files/stroke_3.png'},{rect:new IWRect(419,2,5,264),url:'16_Barramento_e_Portas_de_Tres_Estados_files/stroke_4.png'},{rect:new IWRect(419,266,5,4),url:'16_Barramento_e_Portas_de_Tres_Estados_files/stroke_5.png'},{rect:new IWRect(2,266,417,4),url:'16_Barramento_e_Portas_de_Tres_Estados_files/stroke_6.png'},{rect:new IWRect(-2,266,4,4),url:'16_Barramento_e_Portas_de_Tres_Estados_files/stroke_7.png'}],new IWSize(422,268))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('16_Barramento_e_Portas_de_Tres_Estados_files/16_Barramento_e_Portas_de_Tres_EstadosMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id5');BlogFixupPreviousNext();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
