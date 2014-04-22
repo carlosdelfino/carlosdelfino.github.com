e13352a38451d6508668f15174b05f860a5c7106
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2014-04-21 22:00:47 -0300

    links para projetos adicionado

---
+++ Carlos_Delfino_-_Consultoria_e_Projetos/Projetos/Entradas/2014/4/21_Arduino_Super_Stream_files/21_Arduino_Super_Stream.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20140421
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,196),url:'21_Arduino_Super_Stream_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'21_Arduino_Super_Stream_files/stroke_1.png'},{rect:new IWRect(2,-2,416,4),url:'21_Arduino_Super_Stream_files/stroke_2.png'},{rect:new IWRect(418,-2,4,4),url:'21_Arduino_Super_Stream_files/stroke_3.png'},{rect:new IWRect(418,2,4,196),url:'21_Arduino_Super_Stream_files/stroke_4.png'},{rect:new IWRect(418,198,4,4),url:'21_Arduino_Super_Stream_files/stroke_5.png'},{rect:new IWRect(2,198,416,4),url:'21_Arduino_Super_Stream_files/stroke_6.png'},{rect:new IWRect(-2,198,4,4),url:'21_Arduino_Super_Stream_files/stroke_7.png'}],new IWSize(420,200))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('21_Arduino_Super_Stream_files/21_Arduino_Super_StreamMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id5');BlogFixupPreviousNext();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
