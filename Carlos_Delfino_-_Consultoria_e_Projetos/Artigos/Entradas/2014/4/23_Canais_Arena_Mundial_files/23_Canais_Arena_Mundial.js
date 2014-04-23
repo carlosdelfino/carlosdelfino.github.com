b40890ed682b3c328f2b0483a35f25ec8ecb5b45
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2014-04-23 16:21:50 -0300

    Adicionado link para a transmissão ao vivo dos Canais Arena Mundial

---
+++ Carlos_Delfino_-_Consultoria_e_Projetos/Artigos/Entradas/2014/4/23_Canais_Arena_Mundial_files/23_Canais_Arena_Mundial.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20140423
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,264),url:'23_Canais_Arena_Mundial_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'23_Canais_Arena_Mundial_files/stroke_1.png'},{rect:new IWRect(2,-2,311,4),url:'23_Canais_Arena_Mundial_files/stroke_2.png'},{rect:new IWRect(313,-2,4,4),url:'23_Canais_Arena_Mundial_files/stroke_3.png'},{rect:new IWRect(313,2,4,264),url:'23_Canais_Arena_Mundial_files/stroke_4.png'},{rect:new IWRect(313,266,4,4),url:'23_Canais_Arena_Mundial_files/stroke_5.png'},{rect:new IWRect(2,266,311,4),url:'23_Canais_Arena_Mundial_files/stroke_6.png'},{rect:new IWRect(-2,266,4,4),url:'23_Canais_Arena_Mundial_files/stroke_7.png'}],new IWSize(315,268))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('23_Canais_Arena_Mundial_files/23_Canais_Arena_MundialMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id5');BlogFixupPreviousNext();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
