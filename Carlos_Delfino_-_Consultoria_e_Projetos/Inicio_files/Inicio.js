1ecc74639fbf25db06fe99d7661fd51568cd8c99
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2014-05-09 13:19:57 -0300

    Teste com novo Analytics

--- Carlos_Delfino_-_Consultoria_e_Projetos/Inicio_files/Inicio.js
+++ Carlos_Delfino_-_Consultoria_e_Projetos/Inicio_files/Inicio.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20140415
+// Created by iWeb 3.0.4 local-build-20140509
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,251),url:'Inicio_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'Inicio_files/stroke_1.png'},{rect:new IWRect(2,-2,631,4),url:'Inicio_files/stroke_2.png'},{rect:new IWRect(633,-2,5,4),url:'Inicio_files/stroke_3.png'},{rect:new IWRect(633,2,5,251),url:'Inicio_files/stroke_4.png'},{rect:new IWRect(633,253,5,4),url:'Inicio_files/stroke_5.png'},{rect:new IWRect(2,253,631,4),url:'Inicio_files/stroke_6.png'},{rect:new IWRect(-2,253,4,4),url:'Inicio_files/stroke_7.png'}],new IWSize(635,255))});registry.applyEffects();}
