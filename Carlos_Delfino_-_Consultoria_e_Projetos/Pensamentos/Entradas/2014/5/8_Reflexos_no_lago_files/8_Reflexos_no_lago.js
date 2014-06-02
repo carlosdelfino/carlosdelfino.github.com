1f1939c00c6d5d50109427ba13e06c5748366b05
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2014-06-01 23:20:05 -0300

    Novo pensamento sobre o terrorismo. Algo básico

--- Carlos_Delfino_-_Consultoria_e_Projetos/Pensamentos/Entradas/2014/5/8_Reflexos_no_lago_files/8_Reflexos_no_lago.js
+++ Carlos_Delfino_-_Consultoria_e_Projetos/Pensamentos/Entradas/2014/5/8_Reflexos_no_lago_files/8_Reflexos_no_lago.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20140508
+// Created by iWeb 3.0.4 local-build-20140601
 
 setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,196),url:'8_Reflexos_no_lago_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'8_Reflexos_no_lago_files/stroke_1.png'},{rect:new IWRect(2,-2,416,4),url:'8_Reflexos_no_lago_files/stroke_2.png'},{rect:new IWRect(418,-2,4,4),url:'8_Reflexos_no_lago_files/stroke_3.png'},{rect:new IWRect(418,2,4,196),url:'8_Reflexos_no_lago_files/stroke_4.png'},{rect:new IWRect(418,198,4,4),url:'8_Reflexos_no_lago_files/stroke_5.png'},{rect:new IWRect(2,198,416,4),url:'8_Reflexos_no_lago_files/stroke_6.png'},{rect:new IWRect(-2,198,4,4),url:'8_Reflexos_no_lago_files/stroke_7.png'}],new IWSize(420,200))});registry.applyEffects();}
